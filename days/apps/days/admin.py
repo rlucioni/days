0a4e7e313f44314cf37c7c2eb219258a43be7baa
Renzo Lucioni <renzo@renzolucioni.com>
2016-05-26 00:03:13 -0400

    Appease pylint

--- days/apps/days/admin.py
+++ days/apps/days/admin.py
@@ -1,3 +1,4 @@
+"""Django admin configuration for days models."""
 from django.contrib import admin
 
 from days.apps.days.models import Event
@@ -5,6 +6,7 @@ from days.apps.days.models import Event
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
+    """Admin config for the Event model."""
     list_display = ('date', 'description')
     search_fields = ('description',)
     show_full_result_count = False
