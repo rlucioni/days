c11cbcdd7edfaba08e02737bbc5364ab9857e884
Renzo Lucioni <renzo@renzolucioni.com>
2016-05-25 23:54:58 -0400

    Make the Event admin more useful
    Add date and description columns, and allow searching descriptions

--- days/apps/days/admin.py
+++ days/apps/days/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from days.apps.days.models import Event
 
 
-admin.site.register(Event)
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('date', 'description')
+    search_fields = ('description',)
+    show_full_result_count = False
