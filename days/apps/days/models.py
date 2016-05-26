59a41a76c37c57d4a2eb4ec13f67a7578ce7003c
Renzo Lucioni <renzo@renzolucioni.com>
2016-05-25 22:30:54 -0400

    Add Event model

---
+++ days/apps/days/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Event(models.Model):
+    """Representation of a notable historical event."""
+    date = models.DateField(
+        help_text='When the event occurred.'
+    )
+
+    description = models.TextField(
+        help_text='A description of the event.'
+    )
