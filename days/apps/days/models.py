e149ee5d5b6c76ceae2b5b463405344a8cf4d279
Renzo Lucioni <renzo@renzolucioni.com>
2016-05-29 21:31:17 -0400

    Add Subscriber model

--- days/apps/days/models.py
+++ days/apps/days/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Event(models.Model):
-    """Representation of a notable historical event."""
+    """A notable historical event."""
     # How to perform lookups on this field: https://docs.djangoproject.com/en/1.9/ref/models/querysets/#month
     date = models.DateField(
         help_text='When the event occurred.'
@@ -21,3 +21,25 @@ class Event(models.Model):
 
     def __str__(self):
         return self.date.strftime('%b. %-d %Y')
+
+
+class Subscriber(models.Model):
+    """An individual interested in receiving messages about historical events."""
+    email = models.EmailField(
+        unique=True,
+        help_text='An email address to which messages can be sent.'
+    )
+
+    is_subscribed = models.BooleanField(
+        default=True,
+        help_text='Whether to send emails to this subscriber.'
+    )
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta(object):  # pylint: disable=missing-docstring
+        ordering = ['-modified']
+
+    def __str__(self):
+        return self.email
